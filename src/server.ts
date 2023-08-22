bb778a113c581fd370b485de8948c5b0edfd68b6
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-21 21:25:14 -0300

    feat: adding fastify

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/', async () => {
+  return 'Hello world!'
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('server is running 🚀🚀')
+})
