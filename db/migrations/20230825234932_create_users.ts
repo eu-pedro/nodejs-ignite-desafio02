082ff18b57cdb192aadecb9ce7dece15ec7726b8
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-25 21:02:31 -0300

    running migration to create user table

---
+++ db/migrations/20230825234932_create_users.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.text('name').notNullable()
+    table.text('password').notNullable()
+    table.timestamps(true, true)
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
+}
