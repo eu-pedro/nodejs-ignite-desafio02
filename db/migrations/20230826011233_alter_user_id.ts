4e6ff9cedea6840ad860b48c0b4992cfe97fe6ea
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-25 22:46:00 -0300

    feat: alter table name userID

---
+++ db/migrations/20230826011233_alter_user_id.ts
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('meals', (table) => {
+    table.dropColumn('user_id')
+    table.text('userId').references('users.id')
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('meals', (table) => {
+    table.text('user_id').references('users.id')
+    table.dropColumn('userId')
+  })
+}
+
