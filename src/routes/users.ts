892d59b367663e99035e3432f8f56bef872f7ddb
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-26 22:40:22 -0300

    feat: add all methods to users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -6,7 +6,19 @@ import { randomUUID } from 'crypto'
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const users = await knex('users').select('*')
-    return users
+    return { users }
+  })
+
+  app.get('/:id', async (request) => {
+    const getUserParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getUserParamsSchema.parse(request.params)
+
+    const user = await knex('users').select().where('id', id).first()
+
+    return { user }
   })
 
   app.post('/', async (request, reply) => {
@@ -24,5 +36,36 @@ export async function usersRoutes(app: FastifyInstance) {
     })
 
     return reply.status(201).send()
+  })
+
+  app.delete('/:id', async (request, reply) => {
+    const getIdUserParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getIdUserParamsSchema.parse(request.params)
+
+    await knex('users').where({ id }).del()
+
+    return reply.status(204).send()
+  })
+
+  app.put('/:id', async (request, reply) => {
+    const getIdUserParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const getUserBodySchema = z.object({
+      name: z.string(),
+      password: z.string(),
+    })
+
+    const { name, password } = getUserBodySchema.parse(request.body)
+
+    const { id } = getIdUserParamsSchema.parse(request.params)
+
+    await knex('users').update({ name, password }).where({ id })
+
+    return reply.status(204).send()
   })
 }
