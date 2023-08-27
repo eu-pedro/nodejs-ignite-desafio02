b069e23075fc0c1062f5569676f97f9ca6d7a110
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-27 10:35:53 -0300

    feat: create read and create methods for meals

---
+++ src/routes/meals.ts
@@ -0,0 +1,51 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+import { randomUUID } from 'crypto'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const meals = await knex('meals').select('*')
+
+    return { meals }
+  })
+
+  app.get('/:id', async (request) => {
+    console.log(request.params)
+
+    const getIdMealParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getIdMealParamsSchema.parse(request.params)
+
+    const meal = await knex('meals')
+      .where('meals.userId', id)
+      .join('users', 'users.id', '=', 'meals.userId')
+      .select('meals.*', 'users.name')
+
+    return { meal }
+  })
+
+  app.post('/', async (request, reply) => {
+    const createMealsBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      isHealthy: z.boolean(),
+      userId: z.string(),
+    })
+
+    const { name, description, isHealthy, userId } =
+      createMealsBodySchema.parse(request.body)
+
+    await knex('meals').insert({
+      id: randomUUID(),
+      name,
+      description,
+      isHealthy,
+      userId,
+    })
+
+    return reply.status(201).send()
+  })
+}
