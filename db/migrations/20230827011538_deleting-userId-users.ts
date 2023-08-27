770f87715c370f9b49b17dac0259ba63152b5feb
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-27 10:34:44 -0300

    feat: alter column name users

--- db/migrations/20230827011538_deleting-userId-users.ts
+++ db/migrations/20230827011538_deleting-userId-users.ts
@@ -12,7 +12,6 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.alterTable('users', (table) => {
     table.text('userId')
-    
   })
 
 }
