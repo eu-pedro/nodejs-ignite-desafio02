a01640605f9716346d89c2b3c0717f7066777aa1
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-31 20:40:22 -0300

    feat: creating database test and finished tests

--- src/test/users.spec.ts
+++ src/test/users.spec.ts
@@ -1,4 +1,5 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../app'
 
@@ -10,7 +11,13 @@ describe('User routes', () => {
   afterAll(async () => {
     await app.close()
   })
-  it.only('may be possible to list all users', async () => {
+
+  beforeEach(async () => {
+    // execSync('npm run knex -- migrate:rollback')
+    execSync('npm run knex -- migrate:latest')
+  })
+
+  it('should be able to list all users', async () => {
     const createUserResponse = await request(app.server).post('/users').send({
       name: 'john doe',
       password: 'root123',
@@ -29,5 +36,33 @@ describe('User routes', () => {
         password: 'root123',
       }),
     ])
+  })
+
+  it('should be able to get a specific user', async () => {
+    const createUserResponse = await request(app.server).post('/users').send({
+      name: 'john doe',
+      password: 'root123',
+    })
+
+    const cookies = createUserResponse.get('Set-Cookie')
+
+    const listUserResponse = await request(app.server)
+      .get('/users')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const userId = listUserResponse.body.users[0].id
+
+    const getUserResponse = await request(app.server)
+      .get(`/users/${userId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getUserResponse.body.user).toEqual(
+      expect.objectContaining({
+        name: 'john doe',
+        password: 'root123',
+      }),
+    )
   })
 })
