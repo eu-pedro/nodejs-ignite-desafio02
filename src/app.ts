43a994b0ffa4029e5b5d6d3759b5de7a5cc2175d
Pedro Henrique <pedro.henrike2812@gmail.com>
2023-08-31 21:36:54 -0300

    feat: add cors in my app

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,14 @@ import fastify from 'fastify'
 import { usersRoutes } from './routes/users'
 import { mealsRoutes } from './routes/meals'
 import cookie from '@fastify/cookie'
+import cors from '@fastify/cors'
 
 export const app = fastify()
 
+app.register(cors, {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+})
 app.register(cookie)
 app.register(usersRoutes, {
   prefix: 'users',
